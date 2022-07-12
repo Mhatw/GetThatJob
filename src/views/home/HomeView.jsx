41d572b4e984147c067aed26ee851bc8e7dc4886
Mhatw <contactomhatw@gmail.com>
2022-07-12 18:02:33 -0500

    FEAT: add links for testing routes

--- src/views/home/HomeView.jsx
+++ src/views/home/HomeView.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
 
 export function HomeView() {
   return (
     <>
       <h1>HomeView</h1>
+      <Link to="login/recruiter">Login </Link>
+      <Link to="signup">signup </Link>
       <Outlet />
     </>
   );
--- src/views/home/Login.jsx
+++ src/views/home/Login.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
 
 export function Login() {
   return (
     <>
       <h1>Login</h1>
+      <Link to="recruiter">recruiter </Link>
+      <Link to="professional">professional </Link>
       <Outlet />
     </>
   );
