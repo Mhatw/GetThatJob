962de9f83e1cee7e3995241940b9c4d8f6b2c94a
Mhatw <contactomhatw@gmail.com>
2022-07-14 13:50:39 -0500

    FEAT: add Landing Component first part

--- src/views/home/HomeView.jsx
+++ src/views/home/HomeView.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import { Outlet } from "react-router";
-import { Link } from "react-router-dom";
+import { Outlet, useOutlet } from "react-router";
 import { Header } from "./components";
+import { Landing } from "./Landing";
 
 export function HomeView() {
+  const outlet = useOutlet();
+  console.log(outlet);
   return (
     <>
       <Header />
-      <h1>HomeView</h1>
-      <Link to="/dashboard/recruiter">dashboard-recruiter </Link>
-      <Link to="/dashboard/professional">dashboard-professional</Link>
-      <br />
-      <Link to="login/recruiter">Login </Link>
-      <Link to="signup/recruiter">signup </Link>
-      <Outlet />
+      {outlet ? <Outlet /> : <Landing />}
     </>
   );
 }
