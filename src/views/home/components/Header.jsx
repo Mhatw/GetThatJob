424c922b1ac3fac712aae25389f15a12da4f0726
Mhatw <contactomhatw@gmail.com>
2022-07-14 12:24:45 -0500

    FEAT: add header for homeview

--- src/views/home/components/Header.jsx
+++ src/views/home/components/Header.jsx
@@ -1,31 +1,97 @@
 import React from "react";
-import styled from "styled-components";
 import Logo from "../../../assets/logo.png";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Box,
+  Flex,
+  Spacer,
+  Image,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Link, useNavigate } from "react-router-dom";
 
-const StyledHeader = styled.header`
-  background-color: #fff;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0.5rem 1rem;
-  text-align: center;
-  font-weight: bold;
-  color: #333;
-  div {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 0.5rem;
-    border: 1px solid red;
-  }
-`;
 export function Header() {
+  const navigate = useNavigate();
   return (
     <>
-      <StyledHeader>
-        <img src={Logo} alt="logo" />
-        <div></div>
-      </StyledHeader>
+      <Flex
+        minWidth="max-content"
+        alignItems="center"
+        justifyContent="center"
+        gap="2"
+        px={["0.5rem", "1rem", "2rem"]}
+        py={["0.5rem", "1rem"]}
+        boxShadow="0px 0px 10px rgba(0, 0, 0, 0.1)"
+      >
+        <Link to="/">
+          <Box p="2">
+            <Image w="80px" src={Logo} alt="logo" />
+          </Box>
+        </Link>
+        <Spacer />
+        {/* button group */}
+        <Flex gap="4">
+          {/* signup */}
+          <Menu>
+            <MenuButton
+              as={Button}
+              size="sm"
+              fontSize="0.8rem"
+              rightIcon={<ChevronDownIcon />}
+            >
+              Signup
+            </MenuButton>
+            <MenuList p="1rem">
+              <MenuItem
+                borderRadius="0.2rem"
+                fontWeight="semibold"
+                onClick={() => navigate("/signup/recruiter")}
+              >
+                Recruiter
+              </MenuItem>
+              <MenuItem
+                borderRadius="0.2rem"
+                fontWeight="semibold"
+                onClick={() => navigate("/signup/professional")}
+              >
+                Professional
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          {/* login */}
+          <Menu>
+            <MenuButton
+              as={Button}
+              size="sm"
+              fontSize="0.8rem"
+              colorScheme="blue"
+              rightIcon={<ChevronDownIcon />}
+            >
+              Login
+            </MenuButton>
+            <MenuList p="1rem">
+              <MenuItem
+                borderRadius="0.2rem"
+                fontWeight="semibold"
+                onClick={() => navigate("/login/recruiter")}
+              >
+                Recruiter
+              </MenuItem>
+              <MenuItem
+                borderRadius="0.2rem"
+                fontWeight="semibold"
+                onClick={() => navigate("/login/professional")}
+              >
+                Professional
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
     </>
   );
 }
