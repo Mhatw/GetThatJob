1015a69afafa065fc4ac37b86bca70444b3d9ca7
Mhatw <contactomhatw@gmail.com>
2022-07-21 00:22:59 -0500

    FEAT: add import new components in index component

--- src/views/dashboard/professional/components/index.jsx
+++ src/views/dashboard/professional/components/index.jsx
@@ -1,3 +1,15 @@
 import { JobCard } from "./JobCard";
+import { TagBoxJob } from "./TagsBoxJob";
+import { DescriptionJob } from "./DescriptionJob";
+import { ApplyButton } from "./ApplyButton";
+import { TitleJob } from "./TitleJob";
+import { HeaderCompanyJob } from "./HeaderCompanyJob";
 
-export { JobCard };
+export {
+  JobCard,
+  TagBoxJob,
+  DescriptionJob,
+  ApplyButton,
+  TitleJob,
+  HeaderCompanyJob,
+};
